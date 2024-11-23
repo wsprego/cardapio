0fdbdc79825f326fba31acc409410890bb0f07e8
wsprego <wesleysilva.santos076@gmail.com>
2024-11-23 17:44:56 -0300

    fim

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
+import { useState } from 'react';
 import './App.css'
-import { Card } from './components/card/card';  
-import { FoodData } from './interface/FoodData';
+import { Card } from './components/card/Card';  
+import { useFoodData } from './hooks/useFoodData';
+import { CreateModal } from './components/create-modal/create-modal';
 
 function App() {
-  const data: FoodData[] = [];
+  const {data} = useFoodData();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handlOpenModal= () =>{
+    setIsModalOpen(prev => !prev)
+  }
 
   return (
   
       <div className="container">
         <h1>Cardápio</h1>
         <div className="card-grid">
-          {data.map(foodData => 
+          {data?.map(foodData => 
             <Card 
               price={foodData.price}
               title={foodData.title}
@@ -18,7 +25,8 @@ function App() {
             />
           )}
         </div>
-
+        {isModalOpen && <CreateModal closeModal={handlOpenModal}/>}
+        <button onClick={handlOpenModal}>novo</button>
       </div>
   
   )
--- src/hooks/useFoodData.ts
+++ src/hooks/useFoodData.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosPromise } from "axios"
 import { FoodData } from "../interface/FoodData";
+import { useQuery } from "react-query";
 
 const API_URL = 'http://localhost:8080';
 
@@ -10,5 +11,15 @@ const fetchData = async (): AxiosPromise<FoodData[]> =>{
 
 
 export function useFoodData(){
-    
+    const query = useQuery({
+        queryFn: fetchData,
+        queryKey: ['food-data'],
+        retry:2
+    })
+
+    return {
+        ...query,
+        data: query.data?.data
+    }
 }
+
